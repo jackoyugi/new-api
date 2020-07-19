ba96b1b42739a964dae69ec27170c078d9a67be3
jackoyugi <oyugijack@gmail.com>
2020-07-19 23:16:56 +0300

    added setters, getters, equals, and hascode for the department class

---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,50 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getDepartment() {
+    }
+
+    @Test
+    public void setDepartment() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getNumber_employees() {
+    }
+
+    @Test
+    public void setNumber_employees() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+}
